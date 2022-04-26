9edace9af2bafc77b745c970663ad2b692f75769
ziauljbbc <ziaul.huda@jbbc.co.jp>
2022-04-26 12:10:20 +0600

    multiple image upload started

--- components/MSF/Upload/ImageUpload.js
+++ components/MSF/Upload/ImageUpload.js
@@ -1,4 +1,4 @@
-import React, {useState, useMemo} from "react";
+import React, {useState, useMemo, Component} from "react";
 // import axios from "axios";
 import PropTypes from "prop-types";
 import {useQuery} from "react-query";
@@ -16,49 +16,103 @@ import makeStyles from "@mui/styles/makeStyles";
 // core components
 import GridItem from "components/Grid/GridItem.js";
 import GridContainer from "components/Grid/GridContainer.js";
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 
 import styles from "assets/jss/nextjs-material-dashboard/views/dashboardStyle.js";
 import Box from "@mui/material/Box";
 
+// plugins
+import { Dropzone, FileItem } from "@dropzone-ui/react";
+
 
 export default function ImageUpload() {
-    const [carFeatures, setCarFeatures] = useState([]);
-    const [checkBoxInput, setCheckBoxInput] = useState([]);
+    const [images, setImages] = useState([]);
+    const [fileLimitExceeded, setFileLimitExceeded] = useState(false);
 
     const useStyles = makeStyles(styles);
     const classes = useStyles();
 
-    const onCarFeaturesInputChange = (e) => {
-        const {name} = e.target;
-        const index = checkBoxInput.indexOf(parseInt(name));
-        if (index !== -1) {
-            const newBox = [...checkBoxInput];
-            newBox.splice(index, 1);
-            setCheckBoxInput(newBox);
+    React.useEffect(() => {
+        if (images.length >= 15) {
+            setFileLimitExceeded(true);
         } else {
-            setCheckBoxInput([...checkBoxInput, parseInt(name)]);
+            setFileLimitExceeded(false);
+        }
+    },[images]);
+
+    const onImageUpload = async (file) => {
+        if (file) {
+            const listSize = file.length;
+            const prevListSize = images.length;
+            const newImageLength = listSize - prevListSize;
+            for (let i = listSize - 1; i >= prevListSize; i--) {
+                const image1 = file[i];
+                const imageName = image1.name;
+                console.log(image1);
+                setImages((prev) => [...prev, image1]);
+            }
         }
     };
 
-    React.useEffect(() => {
-        (async () => {
-            try {
-                const response = await fetch(`${process.env.NEXT_PUBLIC_BG_API}cars/features-list/`);
-                const json = await response.json();
-                setCarFeatures(json);
-            } catch (err) {
-                console.error(err);
-            }
-        })();
-    }, []);
+    const onImageDelete = (file) => {
+        const deleteFileName = file.name;
+        const indexOfItemToRemove = images.findIndex((item) => item.name === deleteFileName);
+        if (indexOfItemToRemove === -1) {
+            return;
+        }
+        setImages((list) => [...list.slice(0, indexOfItemToRemove), ...list.slice(indexOfItemToRemove + 1)]);
+    };
+
+    const [files, setFiles] = React.useState([]);
+    const updateFiles = (incommingFiles) => {
+        setFiles(incommingFiles);
+    };
+
 
     return (
         <GridContainer>
             <h2 className={classes.paperTitle}>UPLOAD Car Photo*</h2>
             <GridItem item xs={12} sm={12} md={4}>
-
+                <Dropzone onChange={updateFiles} value={files}>
+                    {files.map((file) => (
+                      <FileItem {...file} preview />
+                    ))}
+                </Dropzone>
+                {/*<DropzoneArea*/}
+                {/*  acceptedFiles={["image/*"]}*/}
+                {/*  dropzoneProps={{ disabled: fileLimitExceeded }}*/}
+                {/*  dropzoneClass={classes.dropZone}*/}
+                {/*  previewGridClasses={{*/}
+                {/*      container: classes.previewContainer,*/}
+                {/*      item: classes.preview,*/}
+                {/*      image: classes.previewImg,*/}
+                {/*  }}*/}
+                {/*  // initialFiles={['https://bhalogari-static.s3.amazonaws.com/media/2005-Honda-Civic-FrontSide_HOCIVSED051_505x375.jpg']}*/}
+                {/*  getPreviewIcon={(file) => {*/}
+                {/*      if (file.file.type.split("/")[0] === "image")*/}
+                {/*          return (*/}
+                {/*            <img*/}
+                {/*              className={classes.previewImg}*/}
+                {/*              role="presentation"*/}
+                {/*              alt="presentation"*/}
+                {/*              src={file.data}*/}
+                {/*            />*/}
+                {/*          );*/}
+                {/*  }}*/}
+                {/*  // open={true}*/}
+                {/*  previewText={false}*/}
+                {/*  dropzoneText={*/}
+                {/*      fileLimitExceeded*/}
+                {/*        ? "You have already uploaded 15 photos"*/}
+                {/*        : "Drag & Drop file you want to upload"*/}
+                {/*  }*/}
+                {/*  filesLimit={15}*/}
+                {/*  maxFileSize={10000000}*/}
+                {/*  onChange={(files) => onImageUpload(files)}*/}
+                {/*  onDelete={(file) => onImageDelete(file)}*/}
+                {/*  showAlerts={["error"]}*/}
+                {/*  showPreviewsInDropzone={false}*/}
+                {/*  showPreviews={true}*/}
+                {/*/>*/}
             </GridItem>
 
         </GridContainer>
