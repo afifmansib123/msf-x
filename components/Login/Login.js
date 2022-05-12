9cdf7d316cb9779e431d17cffae78ee1f52edf20
nasirudd-in <nasir.diudevs@gmail.com>
2022-05-12 17:42:45 +0600

    merchant storefront updated

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -4,20 +4,21 @@ import Link from "next/link";
 function Login() {
   return (
     <div className="flex  h-screen">
-      {/* <h1>hdhfds</h1> */}
-      <div className="m-auto bg-white p-8 border-2 border-orange-600 rounded-lg ">
-        <h1 className="font-bold text-5xl text-orange-600 animate-bounce text-center">
+      <div className="m-auto bg-white  border-2 border-orange-600 rounded-lg shadow-lg ">
+        <h1 className="font-bold text-5xl text-orange-600  text-center px-8 py-5">
           Bhalogari Merchant Storefront
         </h1>
-        <div className="flex justify-center flex-col mt-3 ">
-          <h1 className="text-3xl text-center font-bold">
+        <div className="flex justify-center flex-col mt-3  ">
+          <h1 className="text-3xl text-center font-bold ">
             Sell Your Cars Like a Pro!
           </h1>
-          <ul>
-            <li className="text-3xl mt-3 text-center">
-              <Link href="/msf">Merchant Storefront</Link>
+          <ul className="bg-gray-200">
+            <li className="text-3xl mt-3 text-center bg-gray-200 text-orange-600 ">
+              <Link href="/msf" className="hover:text-white">
+                Merchant Storefront
+              </Link>
             </li>
-            <li className="text-3xl mt-3 text-center">
+            <li className="text-3xl mt-3 text-center bg-gray-200 text-orange-600 mb-5">
               <Link href="/admin">BG Admin Panel</Link>
             </li>
           </ul>
