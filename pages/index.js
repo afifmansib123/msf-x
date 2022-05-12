9ac3029469715ca16eec8407001e40724b75a376
nasirudd-in <nasir.diudevs@gmail.com>
2022-05-12 10:39:44 +0600

    topbar added

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Link from "next/link";
+import TopBar from "../components/TopBar/TopBar";
 
 function Index(props) {
   return (
-    <div className="m-10">
+    <div>
+      <TopBar></TopBar>
       <h1 className="font-bold text-3xl">Bhalogari Merchant Storefront</h1>
       Sell Your Cars Like a Pro!
       <ul>
