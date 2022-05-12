9e8ddefe2f8b1f5f5aa32765676b8f2a237e4226
nasirudd-in <nasir.diudevs@gmail.com>
2022-05-12 14:53:14 +0600

    fixed

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -4,6 +4,7 @@ import Link from "next/link";
 function Login() {
   return (
     <div className="flex  h-screen">
+      {/* <h1>hdhfds</h1> */}
       <div className="m-auto bg-white p-8 border-2 border-orange-600 rounded-lg ">
         <h1 className="font-bold text-5xl text-orange-600 animate-bounce text-center">
           Bhalogari Merchant Storefront
