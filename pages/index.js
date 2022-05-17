b37beb83432a31ca9c666a6f660c4084b83d2762
Chayapol Moemeng <mchayapol@gmail.com>
2022-05-17 09:20:33 +0700

    Fixed sign and error messages

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, forwardRef } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import Login from "../components/Login/Login";
 import TopBar from "../components/TopBar/TopBar";
 import sideImage from "../assets/Header/ss.0bdbffc9.png";
+import { useRouter } from "next/router";
+import MuiAlert from "@mui/material/Alert";
+import Snackbar from "@mui/material/Snackbar";
 
-function Index(props) {
+const Alert = forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+function SiteIndexPage(props) {
+  const router = useRouter();
+  const { error } = router.query;
+  const [open, setOpen] = useState(error);
+  const errorDict = {
+    SessionRequired: "Please sign in",
+  };
+
   return (
     <div className="">
-         {/* <Image  className="float-right ..."  src={sideImage}></Image> */}
-        {/* <TopBar></TopBar> */}
+      {error && (
+        <Snackbar
+          anchorOrigin={{ vertical: "top", horizontal: "center" }}
+          open={open}
+          autoHideDuration={6000}
+          onClose={() => setOpen(false)}
+        >
+          <Alert onClose={() => setOpen(false)} severity="error" sx={{ width: "100%" }}>
+            {errorDict[error]}
+          </Alert>
+        </Snackbar>
+      )}
+
+      {/* <Image  className="float-right ..."  src={sideImage}></Image> */}
+      {/* <TopBar></TopBar> */}
       <Login />
     </div>
   );
 }
 
 export async function getStaticProps(context) {
-  // console.log("LANDING_PAGE", process.env.LANDING_PAGE);
   return {
-    props: {
-      // landingPage: process.env.LANDING_PAGE ? process.env.LANDING_PAGE : null,
-    },
+    props: {},
   };
 }
 
-export default Index;
+export default SiteIndexPage;
