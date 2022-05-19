6e7cf2b78faecea4d325b3f798b34898b9550e7b
Chayapol Moemeng <mchayapol@gmail.com>
2022-05-19 08:37:44 +0700

    Minor fix

--- pages/msf/listing.js
+++ pages/msf/listing.js
@@ -1,33 +1,32 @@
-import React, {useState, useEffect} from "react";
-import {useRouter} from "next/router";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 import makeStyles from "@mui/styles/makeStyles";
+
 // layout for this page
 import MSF from "layouts/MSF.js";
 import styles from "assets/jss/nextjs-material-dashboard/views/iconsStyle.js";
-// import { useSession } from "next-auth/client";
-import {useSession} from "next-auth/react";
+import { useSession } from "next-auth/react";
 import axios from "axios";
-import UploadedCarsList from "../../components/UploadedCarsList/UploadedCarsList";
-
+import UploadedCarsList from "/components/UploadedCarsList/UploadedCarsList";
+import { getSession } from "next-auth/react";
 import prisma from "/PrismaConnect";
 // import { PrismaClient } from "@prisma/client";
 
-export default function listing() {
+export default function ListingPage(props) {
+  const { cars } = props;
   const useStyles = makeStyles(styles);
   const classes = useStyles();
   const [items, setItems] = useState([]);
 
-  const {data: session, status} = useSession()
+  const { data: session, status } = useSession();
   console.log("Session Status", status);
   // console.log(session.user);
   const id = session.token.id;
   React.useEffect(async () => {
     try {
-      const {data} = await axios.get(
-        `${process.env.NEXT_PUBLIC_BG_API}cars/user-car-list/${id}/`
-      );
+      const { data } = await axios.get(`${process.env.NEXT_PUBLIC_BG_API}cars/user-car-list/${id}/`);
       console.log(data);
-      setItems(data.results)
+      setItems(data.results);
     } catch (err) {
       console.error(err);
     }
@@ -36,13 +35,13 @@ export default function listing() {
   return (
     <div>
       <div className="grid lg:grid-cols-4 sm:grid-cols-1 md:grid-cols-2 justify-center items-center rounded-lg border bg-white sm:p-8 p-2 sm:mx-32 lg:mx-0 md:mx-4 mt lg:space-x-1 gap-5 ">
-        {data.map((data) => (
+        {cars.map((data) => (
           <UploadedCarsList key={data.car_id} data={data}></UploadedCarsList>
         ))}
       </div>
     </div>
   );
-};
+}
 
 export async function getServerSideProps(context) {
   const session = await getSession(context);
@@ -56,8 +55,8 @@ export async function getServerSideProps(context) {
       CarsApp_carmanufacturer: true,
       CarsApp_carmodel: true,
       CarsApp_cartype: true,
-      CarsApp_carfuel_CarsApp_car_car_fuel_idToCarsApp_carfuel:true,
-      CarsApp_carimage:true
+      CarsApp_carfuel_CarsApp_car_car_fuel_idToCarsApp_carfuel: true,
+      CarsApp_carimage: true,
     },
   });
 
@@ -66,7 +65,7 @@ export async function getServerSideProps(context) {
   console.log(cars[0]);
   // console.log("session", session);
   console.log("user_id", user_id);
-  return { props: { data: cars } };
+  return { props: { cars: cars } };
 }
 
 /*
@@ -84,5 +83,5 @@ export async function getServerSideProps() {
 >>>>>>> uat
 }
 */
-listing.layout = MSF;
-listing.auth = true;
+ListingPage.layout = MSF;
+ListingPage.auth = true;
