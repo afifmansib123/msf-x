0b18a63cf01ba47e4ea06935bbc2407a492116b5
greatkub <71839217+greatkub@users.noreply.github.com>
2022-05-26 10:27:47 +0700

    complete merchants filter (#161)
    Co-authored-by: paranan <u6135118@au.edu>

--- pages/admin/merchants.js
+++ pages/admin/merchants.js
@@ -1,8 +1,6 @@
 import * as React from "react";
 import { useRouter } from "next/router";
-
 import Link from "next/link";
-
 // layout for this page
 import Admin from "layouts/Admin.js";
 // core components
@@ -14,7 +12,6 @@ import CardHeader from "components/Card/CardHeader.js";
 import CardBody from "components/Card/CardBody.js";
 import { PrismaClient } from "@prisma/client";
 import axios from "axios";
-
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 // import StyledTableCell from "@mui/material/StyledTableCell";
@@ -33,7 +30,7 @@ import Checkbox from '@mui/material/Checkbox';
 import FormControl from '@mui/material/FormControl';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
-
+import { isTemplateExpression } from "typescript";
 const styles = {
   cardCategoryWhite: {
     "&,& a,& a:hover,& a:focus": {
@@ -63,25 +60,54 @@ const styles = {
     },
   },
 };
-
 function MerchantPage(props) {
+  const [merList, setMerList] = React.useState([])
   const useStyles = makeStyles(styles);
-  const classes = useStyles();
-  const [page, setPage] = React.useState(1);
-  const [merchants, setMerchants] = React.useState([]);
-  const router = useRouter();
+  const [itemList, setItemList] = React.useState([])
   const { packages, payments } = props;
   const [checked1, setChecked1] = React.useState(true);
   const [checked2, setChecked2] = React.useState(true);
   const [checked3, setChecked3] = React.useState(true);
   const [checked4, setChecked4] = React.useState(true);
-  const arr = []
+  const [checkbox1, setCheckedBox1] = React.useState(false)
+  const [checkbox2, setCheckedBox2] = React.useState(false)
+  const [page, setPage] = React.useState(1);
+  const [state, setState] = React.useState({
+    NonSub: false,
+    Sub: false,
+  });
+  const arrobj = {
+    name: "",
+    phone_num: "",
+    subscription: "",
+    last_login: "",
+    merchantid: ""
+  }
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+  const onCheckChange = (event) => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.checked,
+    });
+  }
+  const { NonSub, Sub } = state;
   const handleChange = (e, value) => {
     setPage(value);
   };
-  const totalCount = props.merchantCount || 0;
-  const totalPage = Math.ceil(totalCount / 10) - 1;
-
+  const onCheckChangeBox1 = (event) => {
+    setCheckedBox1(event.target.checked)
+  }
+  const onCheckChangeBox2 = (event) => {
+    setCheckedBox2(event.target.checked)
+  }
+  var totalCount = props.merchantCount || 0;
+  var totalPage = Math.ceil(totalCount / 10);
   const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
       backgroundColor: "#ff6600",
@@ -93,209 +119,96 @@ function MerchantPage(props) {
       fontSize: 14,
     },
   }));
-
   React.useEffect(() => {
-    const merchantlist = axios.get(`/api/merchants?page=${page}&filter1=${checked1}&filter2=${checked2}&filter3=${checked3}&filter4=${checked4}`).then((v) => {
-      let { merchants } = v.data;
-     
-      merchants = merchants || [];
-      let found1 = false;
-
-      // var list = [];
-      // const mer = merchants.map((mc, k) => {
-      //   console.log("mc", mc)
-      //   return payments.map((pa, i) => {
-      //     console.log("pa", pa)
-      //     if (mc.id === pa.user_id_id) {
-      //       return packages.map((p, k) => {
-      //         console.log("pkg", p)
-      //         if(pa.package_id_id === p.id && p.package_type === "subscription") {
-      //           return list.push(p.package_name)
-      //         } else {
-      //           return list.push("Non-Sub")
-      //         }
-      //       })
-      //     }
-      //   });
-      // });
-      // console.log("Please work", list)
-
-      // const payment_user = payments.map((v, i) => {
-      //   if(m.id === v.user_id_id &&
-      //     v.package_id_id === j.id && j.package_type === "subscription") {
-      //     return []
-      //   }
-      // })
-
-      let result = merchants.map((m, index) => {
-        let found2 = false;
-      
-        const foundFunc = (found2) => {
-          if (!found2) {
-            found2 = true;
-            return <div>Non Subscription</div>
-          }
+    let { merchants } = props;
+    merchants = merchants || [];
+    let found1 = false;
+    let result = merchants.map((m, index) => {
+      var arrobj = {
+        name: "",
+        phone_num: "",
+        subscription: "",
+        last_login: "",
+        merchantid: ""
+      }
+      let found2 = false;
+      const foundFunc = (found2) => {
+        if (!found2) {
+          found2 = true;
+          arrobj
+          return "Non Subscription"
         }
-        return (
-
-          <TableBody key={index}>
-            <TableRow>
-              <StyledTableCell>
-                {m.first_name !== null ? (
-                  <a href={`/admin/merchants/${m.id}`}>
-                    {m.first_name} {m.last_name}
-                  </a>
-                ) : (
-                  <div>Not Specify</div>
-                )}
-              </StyledTableCell>
-
-              <StyledTableCell>{m.contact_number}</StyledTableCell>
-              <StyledTableCell>
-                {
-
-                  payments.map((j) => {
-
-                    if (m.id == j.user_id_id) {
-
-                      return packages.map((k) => {
-                        if (
-                          j.package_id_id == k.id &&
-                          k.package_type == "subscription" && !found1
-                        ) {
-                          console.log("k", k);
-                          console.log("pakage", k.package_name);
-                          found1 = true;
-                          found2 = true;
-                          console.log("arr", arr.slice(-1).pop())
-                          // arr.push(k.package_name);
-                          return <div>{k.package_name}</div>
-                          //  return arr[arr.length-1]}
-
-                        }
-                        // return <div>Non Subscribe</div>
-                      });
-
-                    }
-                    // if (!found2) {
-                    //   found2 = true;
-                    //   return <div>non-subscribe</div>
-                    // } 
-
-                  })
-
-
-
-                  // payments.map((j) => {
-                  //   if (m.id == j.user_id_id) {
-                  //     console.log("j", j);
-                  //     return packages.map((k) => {
-                  //       if (
-                  //         j.package_id_id == k.id &&
-                  //         k.package_type == "subscription"
-                  //       ) {
-                  //         console.log("k", k);
-                  //         console.log("pakage", k.package_name);
-                  //         // found = true;
-                  //         console.log("arr", arr.slice(-1).pop())
-                  //        // arr.push(k.package_name);
-                  //         return <div>{k.package_name}</div>
-                  //       //  return arr[arr.length-1]}
-
-                  //       }
-                  //       // return <div>Non Subscribe</div>
-                  //     });
-
-                  //   } else {
-                  //     return 
-                  //   } 
-
-                  // }) || "UNKNOWN"
-
-
-                  // packages.map(k => {
-                  //   console.log(k)
-                  // console.log(m.MerchantStorefront_package[0].package_name)
-                  // {payments.map(j => {
-                  //   console.log("J", j)
-                  //   if (m.id == j.user_id_id &&
-                  //     j.package_id_id === k.id && k.package_type === "subscription")
-                  //     console.log("ID", j.id)
-                  //      return (<h3>{k.package_name}</h3>)
-                  // m.MerchantStorefront_package.length != 0 ?
-                  //  m.MerchantStorefront_package[0].package_name
-                  //   : "Non-subscribe"
-                  // })}
-                  // })
-                }
-                {foundFunc(found2)}
-              </StyledTableCell>
-              <StyledTableCell> {m.last_login}</StyledTableCell>
-              <StyledTableCell>
-                {" "}
-                <Button
-                  color="warning"
-                  variant="outlined"
-                  href={`/admin/merchants/${m.id}`}
-                >
-                  Details
-                </Button>
-              </StyledTableCell>
-            </TableRow>
-          </TableBody>
-        );
-      });
-      setMerchants(result);
-    });
-  }, [page,checked1, checked2]);
-
-  const handleChange3 = (event) => {
-    setChecked1(event.target.checked);
-};
-const handleChange4 = (event) => {
-    setChecked2(event.target.checked);
-};
-const handleChange5 = (event) => {
-  setChecked3(event.target.checked);
-};
-const handleChange6 = (event) => {
-  setChecked4(event.target.checked);
-};
-function test() {
-  console.log(checked1,checked2,checked3,checked4)
-}
-
+      }
+      m.first_name !== null ? (
+        arrobj.name = m.first_name + " " + m.last_name
+      ) : (
+        arrobj.name = "Not Specify"
+      )
+      arrobj.phone_num = m.contact_number
+      var a = ""
+      payments.map((j) => {
+        if (m.id == j.user_id_id) {
+          return packages.map((k) => {
+            if (
+              j.package_id_id == k.id &&
+              k.package_type == "subscription" && !found1
+            ) {
+              found1 = true;
+              found2 = true;
+              return a = k.package_name
+            }
+          });
+        }
+      })
+      if (a != "") {
+        arrobj.subscription = a
+      } else {
+        arrobj.subscription = foundFunc(found2)
+      }
+      arrobj.last_login = m.last_login
+      arrobj.merchantid = m.id
+      merList.push(arrobj)
+    })
+    setItemList(merList)
+    setMerList(merList)
+  }, [])
+  function filterSub(box1, box2) {
+    if (box1 == false && box2 == false || box1 == true && box2 == true) {
+      totalCount = itemList.length || 0; // 59
+      totalPage = Math.ceil(itemList.length / 10); // 5
+      return itemList.slice((page - 1) * 10, 10 * page)
+    }
+    if (box1 == true && box2 == false) {
+      const z = itemList.filter(function (el) {
+        return (el.subscription == "Non Subscription");
+      })
+      totalCount = z.length || 0;
+      totalPage = Math.ceil(z.length / 10);
+      return z.slice((page - 1) * 10, 10 * page)
+    } else {
+      const a = itemList.filter(function (el) {
+        return (el.subscription != "Non Subscription");
+      })
+      totalCount = a.length || 0;
+      totalPage = Math.ceil(a.length / 10);
+      return a.slice((page - 1) * 10, 10 * page)
+    }
+  }
   return (
     <>
-      {/* <Button onClick={() => { console.log(merchants) }}>test</Button> */}
       <h1 className="text-4xl font-semibold text-center mb-4">Merchants</h1>
-
-      {/* <Button onClick={()=>test()}>test</Button> */}
-
-      {/* <div>
-
+      <div>
         <FormGroup row>
           <FormControlLabel
-            control={<Checkbox checked={checked1} onChange={handleChange3}  />}
+            control={<Checkbox onChange={onCheckChangeBox1} name="NonSub" />}
             label="Non Subscription"
           />
           <FormControlLabel
-            control={<Checkbox checked={checked2} onChange={handleChange4}  />}
-            label="Silver"
-          />
-          <FormControlLabel
-            control={<Checkbox checked={checked3} onChange={handleChange5}  />}
-            label="Gold"
-          />
-          <FormControlLabel
-            control={<Checkbox checked={checked4} onChange={handleChange6}  />}
-            label="Diamond"
+            control={<Checkbox onChange={onCheckChangeBox2} name="Sub" />}
+            label="Subscription"
           />
         </FormGroup>
-
-      </div> */}
-
-
+      </div>
       <GridContainer>
         <GridItem xs={12} sm={12} md={12}>
           <TableContainer component={Paper}>
@@ -309,7 +222,26 @@ function test() {
                   <StyledTableCell></StyledTableCell>
                 </TableRow>
               </TableHead>
-              {merchants}
+              <TableBody>
+                {filterSub(checkbox1, checkbox2).map((item, index) => {
+                  return (
+                    <TableRow>
+                      <StyledTableCell>{item.name}</StyledTableCell>
+                      <StyledTableCell>{item.phone_num}</StyledTableCell>
+                      <StyledTableCell>{item.subscription}</StyledTableCell>
+                      <StyledTableCell>{item.last_login}</StyledTableCell>
+                      <StyledTableCell> <Button
+                        color="warning"
+                        variant="outlined"
+                        href={`/admin/merchants/${item.merchantid}`}
+                      >
+                        Details
+                      </Button></StyledTableCell>
+                    </TableRow>
+                  )
+                })}
+              </TableBody>
+              {/* {merchants} */}
             </Table>
           </TableContainer>
         </GridItem>
@@ -332,7 +264,6 @@ function test() {
     </>
   );
 }
-
 export async function getServerSideProps() {
   const prisma = new PrismaClient();
   var allMerchants = await prisma.UsersApp_customuser.findMany({
@@ -346,63 +277,21 @@ export async function getServerSideProps() {
       updated_at: "desc",
     },
   });
-
-  // const users = await prisma.UsersApp_customuser.findMany();
-
-  // let newlo = []
-  // for (const user of users) {
-  //   const pay = await prisma.MerchantStorefront_paymenthistory.findFirst({
-  //     where: {
-  //       user_id_id: user.id,
-  //     },
-  //     include:{
-  //       MerchantStorefront_package: {
-  //         select:{
-  //           id:true,
-  //           package_name:true,
-  //           package_type:true,
-  //         },
-  //       },
-  //     },
-  //   }).then(() => {
-  //     const pkg = await prisma.MerchantStorefront_package.findFirst({
-  //       where: {
-  //         id: pay.package_id_id
-  //       }
-  //     })
-  //   });
-  // }
-
   allMerchants = JSON.parse(
     JSON.stringify(allMerchants, (key, value) =>
       typeof value === "bigint" ? value.toString() : value
     )
   );
-
   packages = JSON.parse(
     JSON.stringify(packages, (key, value) =>
       typeof value === "bigint" ? value.toString() : value
     )
   );
-
   payments = JSON.parse(
     JSON.stringify(payments, (key, value) =>
       typeof value === "bigint" ? value.toString() : value
     )
   );
-
-  console.log("Original", allMerchants[0]);
-  // allCars = allCars.map((car) => {
-  //   // Convert each fields.
-  //   // Ex: the id was a BigInt and cannot be serialised to JSON, so convert to Number
-  //   let car2 = {};
-  //   car2["id"] = Number(car.id);
-  //   car2["maker"] = car.CarsApp_carmanufacturer.maker_name;
-  //   car2["fixed_price"] = Number(car.fixed_price);
-  //   car2["view_count"] =
-  //   return car2;
-  // });
-  // console.log("Transformed", allMerchants[0]);
   let merchantCount = await prisma.UsersApp_customuser.count();
   return {
     props: {
@@ -413,8 +302,6 @@ export async function getServerSideProps() {
     },
   };
 }
-
 MerchantPage.layout = Admin;
 MerchantPage.auth = true;
-
 export default MerchantPage;
--- pages/admin/merchants/[id].js
+++ pages/admin/merchants/[id].js
@@ -1,13 +1,10 @@
 import React from "react";
-
 import { useState, useRef} from "react";
-
 import makeStyles from '@mui/styles/makeStyles';
 import styles from "assets/jss/nextjs-material-dashboard/views/dashboardStyle.js";
 import GridItem from "components/Grid/GridItem.js";
 import GridContainer from "components/Grid/GridContainer.js";
 import { useRouter } from "next/router";
-
 import Table from "components/Table/Table.js";
 import Card from "components/Card/Card.js";
 import CardHeader from "components/Card/CardHeader.js";
@@ -25,7 +22,6 @@ import IconButton from '@mui/material/IconButton';
 import Image from 'next/image'
 import { prisma } from "@prisma/client";
 import Chip from '@mui/material/Chip';
-
 import Stack from '@mui/material/Stack'
 import { useForm } from "react-hook-form";
 import { useS3Upload } from "next-s3-upload";
@@ -36,18 +32,11 @@ import Snackbar from "@mui/material/Snackbar";
 import MuiAlert from "@mui/material/Alert";
 import Button from '@mui/material/Button';
 import { Router } from "next/router";
-
 const Alert = React.forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 ;
-
-
-
-
-
 function MerchantCardDetail(props) {
-
   const [open, setOpen] = useState(false);
   const [selectedCard, setSelected] = useState(null);
   const [type, setType] = useState("");
@@ -59,7 +48,6 @@ function MerchantCardDetail(props) {
   const [data, setData] = useState({});
   const [editFlag, setEditFlag] = useState(false);
   const [showList, setshowList] = useState([]);
-
   let { uploadToS3 } = useS3Upload();
   // let { store } = props;
   // const { store, setStore } = useState(defaultStore);
@@ -70,13 +58,10 @@ function MerchantCardDetail(props) {
   const [logoUrl, setLogoUrl] = useState(store.logo_url);
   const [backdropUrl, setBackdropUrl] = useState(store.backdrop_url);
   const router = useRouter();
-
   const { data: session, status } = useSession();
-
   // console.log("useSession", session);
   // const { token } = session;
   // const { id } = token;
-
   // const handleSubmit = () => {
   //   setOpen(false);
   //   const reason = inputElement.current.value;
@@ -91,18 +76,11 @@ function MerchantCardDetail(props) {
   // }
   const handleEdit = (e) => {
     setEditFlag(e);
-
-
   };
-
   //Profile Functionality here
-
-
   const handleEditMode = () => {
     setReadOnly(false);
-    
   };
-
   const onSubmitUpdate = async (data) => {
     console.log(data);
     // Use PUT to update non-image data
@@ -114,7 +92,6 @@ function MerchantCardDetail(props) {
     }
     setReadOnly(true);
   };
-
   let handleLogoChange = async (e) => {
     let file = e.target.files[0];
     console.log("file", file);
@@ -136,7 +113,6 @@ function MerchantCardDetail(props) {
     // setStore(patchedStore);
     setLogoUrl(url);
   };
-
   let handleBackdropChange = async (e) => {
     // console.log(file);
     let file = e.target.files[0];
@@ -159,13 +135,6 @@ function MerchantCardDetail(props) {
     // setStore(patchedStore);
     setBackdropUrl(url);
   };
-
-
-
-
-
-
-
   const showedDetail = props.tableData.map((value, index) => {
     React.useEffect(async () => {
       try {
@@ -182,30 +151,21 @@ function MerchantCardDetail(props) {
         console.error(err);
       }
     }, [editFlag]);
-  
     if (value.is_paid == true){ return (
       <>
         <div className="flex ml-6">
           <div class="w-48"><img src={value.image_url}></img></div>
           <div className="right ml-10">
-
             <h3 className="text-xl font-bold mb-2 mt-2">{value.first_name} {value.last_name} ({value.gender}) </h3>
             <h3 className="text-base">Contact: {value.contact_number}</h3>
             <h3 className="text-base ">Email: {value.email}</h3>
             <h3 className="text-base ">Address: {value.address}</h3>
-
-
             <h3 className="text-lg font-medium mt-4">Subscription: </h3>
-
-
-
-
             {packages.map(i => {
               return (<h3 className="text-base ">{i.package_name} ({i.description})</h3>)
             })}
             <div className="font-bold mt-2 ">
               <Stack direction="row" spacing={1}>
-
                 <Chip label="Paid" color="success" />
               </Stack>
             </div>
@@ -217,18 +177,15 @@ function MerchantCardDetail(props) {
         <div className="flex ml-6">
           <div class="w-48"><img src={value.image_url}></img></div>
           <div className="right ml-10">
-
             <h3 className="text-xl font-bold mb-2 mt-2">{value.first_name} {value.last_name} ({value.gender}) </h3>
             <h3 className="text-base">Contact: {value.contact_number}</h3>
             <h3 className="text-base ">Email: {value.email}</h3>
             <h3 className="text-base ">Address: {value.address}</h3>
-
             <div className="flex font-bold mt-2 ">
               <h2 className="my-0 font-sans text-lg from-neutral-500 font-semibold p-0 m-0 mb-4 mr-2">
                 Profile Type:
               </h2>
               <Stack direction="row" spacing={1}>
-
                 <Chip label={value.individual_user ? "Individual" : "Business"} color="warning" />
               </Stack>
             </div>
@@ -236,12 +193,9 @@ function MerchantCardDetail(props) {
         </div>
       </>
     }
-    
   });
-
   return (
     <>
-
       <GridItem xs={12} sm={12} md={6}>
         <Card >
           <CardHeader color={"bhalogari"} className={"m-3"}>
@@ -251,18 +205,23 @@ function MerchantCardDetail(props) {
             </p> */}
           </CardHeader>
           <CardBody>
-
-           
             {editFlag ? (
               props.tableData.map((v,i)=>{
-              return(  <ProfileForm data={data} date={date} userID={v.id} handleEdit={handleEdit} />
-             
+                function parse(text) {
+                  return JSON.parse(text, (_, value) => {
+                      if (typeof value === 'string') {
+                          const m = value.match(/(-?\d+)n/);
+                          if (m && m[0] === value) {
+                              value = BigInt(m[1]);
+                          }
+                      }
+                      return value;
+                  });
+              }
+                var id = parse(v.id)
+              return(  <ProfileForm data={data} date={date} userId={id} />
               )
-            
-              
               })
-
-       
       ) : (
         <>
         <div className="container">
@@ -281,7 +240,6 @@ function MerchantCardDetail(props) {
           {showedDetail}
         </>
       )}
-
           </CardBody>
         </Card>
         <Card >
@@ -333,7 +291,6 @@ function MerchantCardDetail(props) {
                       readOnly: readOnly,
                     }}
                   />
-
                   <TextField
                     required
                     label="Phone"
@@ -343,7 +300,6 @@ function MerchantCardDetail(props) {
                       readOnly: readOnly,
                     }}
                   />
-
                   <TextField
                     required
                     multiline
@@ -384,7 +340,6 @@ function MerchantCardDetail(props) {
                 <div className="mt-4 p-4 grid gap-4 grid-cols-2 bg-white">
                   <div>
                     {/* <FileInput onChange={handleLogoChange} /> */}
-
                     <Button variant="contained" component="label">
                       Upload Logo
                       <input type="file" hidden onChange={handleLogoChange} />
@@ -392,7 +347,6 @@ function MerchantCardDetail(props) {
                   </div>
                   <div>
                     {/* <FileInput onChange={handleBackdropChange} /> */}
-
                     <Button variant="contained" component="label">
                       Upload Backdrop
                       <input type="file" hidden onChange={handleBackdropChange} />
@@ -403,11 +357,6 @@ function MerchantCardDetail(props) {
                 </div>
               </div>
             </div>
-
-
-
-           
-
           </CardBody>
         </Card>
       </GridItem>
@@ -424,28 +373,20 @@ function MerchantCardDetail(props) {
     </>
   );
       }
-      
-
-
-
 export async function getServerSideProps(context) {
-
   const prisma = new PrismaClient();
   const id = context.params.id;
   const tableData = await getUserDetail(parseInt(id));
   var packages = await prisma.MerchantStorefront_package.findMany({
-
   })
   var userStore = await prisma.MerchantStorefront_store.findFirst({
     where: {
       owner_user_id: BigInt(id),
     },
   });
-
   userStore = JSON.parse(
     JSON.stringify(userStore, (key, value) => (typeof value === "bigint" ? value.toString() : value))
   );
-
   // console.log("userStore", userStore);
   // console.log("\tObject Check:", userStore && Object.keys(userStore).length);
   if (!userStore) {
@@ -453,22 +394,17 @@ export async function getServerSideProps(context) {
     // check whether the store is empty, {}
     // No store found, create new one
     // console.debug(`\tNo store found. Creating a new one for [${session.user.name}]`);
-    
     userStore = await prisma.MerchantStorefront_store.create({
       data: {
-
         owner_user_id:  BigInt(id),
         // created_at: new Date(),
         // updated_at: new Date(),
       },
     });
-
     userStore = JSON.parse(
       JSON.stringify(userStore, (key, value) => (typeof value === "bigint" ? value.toString() : value))
     );
    }
-
-
   packages = JSON.parse(
     JSON.stringify(packages, (key, value) => (typeof value === "bigint" ? value.toString() : value))
   );
@@ -479,25 +415,16 @@ export async function getServerSideProps(context) {
       store: userStore,
     }
   }
-
- 
 }
 async function getUserDetail(user_id) {
   const prisma = new PrismaClient();
   const data = await prisma.UsersApp_customuser.findMany({
     where: {
       id: BigInt(user_id) || null,
-
     },
     include: {
-
       MerchantStorefront_paymenthistory: true,
-
     },
-
-
-      
-
   }).catch((err) => {
     throw new Error(err);
   });
@@ -506,12 +433,5 @@ async function getUserDetail(user_id) {
   );
   return parsedData;
 }
-
 MerchantCardDetail.layout = Admin;
 export default MerchantCardDetail;
-
-
-
-
-
-
