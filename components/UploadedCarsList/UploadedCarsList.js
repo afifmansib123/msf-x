c04c8b4f2372929c53e85a0a591d9c293ee3c910
nasirudd-in <nasir.diudevs@gmail.com>
2022-05-30 11:53:51 +0600

    car status added to car list

--- components/UploadedCarsList/UploadedCarsList.js
+++ components/UploadedCarsList/UploadedCarsList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 
 const UploadedCarsList = ({ data }) => {
   const {
@@ -15,6 +15,7 @@ const UploadedCarsList = ({ data }) => {
     transmission_type,
     CarsApp_cartype: car_type,
     registration_year,
+    car_status,
   } = data;
 
   const router = useRouter();
@@ -22,17 +23,45 @@ const UploadedCarsList = ({ data }) => {
     router.push({
       pathname: `/msf/edit-list/${data.id}`,
       // query: { cid: car_id },
-    })
-  }
+    });
+  };
 
   return (
     <div>
       <div className="w-full mb-2  bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-        {images.length > 0 ? (
-          <img className="rounded-t-lg h-36 w-full object-cover" src={images[0].image_url} alt="Landscape picture" />
-        ) : (
-          <img className="rounded-t-lg h-36 mx-auto object-cover" src="/assets/img/listing/picture.svg" alt="Landscape picture" />
-        )}
+        <div className="relative overflow-hidden">
+          {images.length > 0 ? (
+            <img
+              className="rounded-t-lg h-36 w-full object-cover"
+              src={images[0].image_url}
+              alt="Landscape picture"
+            />
+          ) : (
+            <img
+              className="rounded-t-lg h-36 mx-auto object-cover"
+              src="/assets/img/listing/picture.svg"
+              alt="Landscape picture"
+            />
+          )}
+          {car_status === "active" && (
+            <p className="text-sm absolute top-0 right-0 py-1 px-2 bg-orange-500 rounded text-white m-2">
+              Approved
+            </p>
+          )}
+          {car_status === "P" && (
+            <p className="text-sm absolute top-0 right-0 p-2 bg-orange-500 rounded text-white m-2">
+              Pending
+            </p>
+          )}
+          {car_status === "R" && (
+            <p className="text-sm absolute top-0 right-0 p-2 bg-orange-500 rounded text-white m-2">
+              Rejected
+            </p>
+          )}
+          {/* <p className="absolute top-0 right-0 p-2 bg-orange-500 rounded text-white m-2">
+            {car_status}
+          </p> */}
+        </div>
         <div className="px-5 py-5">
           <h6 className="mb-1 text-sm tracking-tight text-gray-600 dark:text-white font-sans ">
             {car_manufacturer.maker_name}
@@ -43,35 +72,58 @@ const UploadedCarsList = ({ data }) => {
 
           <div className="flex space-x-1 mb-1">
             <img src="/assets/CarListPageIcons/taka.fb378b29.svg"></img>
-            <h5 className=" text-sm font-bold tracking-tight text-gray-900 dark:text-white">{fixed_price}</h5>
+            <h5 className=" text-sm font-bold tracking-tight text-gray-900 dark:text-white">
+              {fixed_price}
+            </h5>
           </div>
 
           <h6 className="mb-1 text-sm tracking-tight text-gray-900 dark:text-white">
             Category: <span className="font-bold">{car_type.type_name}</span>
           </h6>
-          <h6 className="mb-1 text-sm tracking-tight text-gray-900 dark:text-white">Model Year: {car_year}</h6>
+          <h6 className="mb-1 text-sm tracking-tight text-gray-900 dark:text-white">
+            Model Year: {car_year}
+          </h6>
+          <h6 className="mb-1 text-sm tracking-tight text-gray-900 dark:text-white">
+            Model Year: {car_status}
+          </h6>
           <div className="">
             <div className="flex items-center space-x-1 ">
               <div className="flex  items-center  flex-col w-1/4">
-                <img src="/assets/CarListPageIcons/milage.svg" className=" bg-gray-200 rounded-full width-1" />
+                <img
+                  src="/assets/CarListPageIcons/milage.svg"
+                  className=" bg-gray-200 rounded-full width-1"
+                />
 
                 <p className="text-sm">{mileage}</p>
               </div>
               <div className="flex  items-center flex-col w-1/4">
-                <img src="/assets/CarListPageIcons/fuel.svg" className=" bg-gray-200 rounded-full width-1" />
+                <img
+                  src="/assets/CarListPageIcons/fuel.svg"
+                  className=" bg-gray-200 rounded-full width-1"
+                />
 
                 <p className="text-sm">{car_fuel.fuel_type}</p>
               </div>
               <div className="flex items-center flex-col w-1/4">
-                <img src="/assets/CarListPageIcons/calendar.svg" className=" bg-gray-200 rounded-full width-1" />
+                <img
+                  src="/assets/CarListPageIcons/calendar.svg"
+                  className=" bg-gray-200 rounded-full width-1"
+                />
                 <p className="text-sm">{registration_year}</p>
               </div>
               <div className="flex  justify-center items-center flex-col w-1/4">
-                <img src="/assets/CarListPageIcons/transmission.svg" className=" bg-gray-200 rounded-full width-1"/>
+                <img
+                  src="/assets/CarListPageIcons/transmission.svg"
+                  className=" bg-gray-200 rounded-full width-1"
+                />
 
-                {transmission_type === "A" && <p className="text-sm">Automatic</p>}
+                {transmission_type === "A" && (
+                  <p className="text-sm">Automatic</p>
+                )}
                 {transmission_type === "M" && <p className="text-sm">Manual</p>}
-                {transmission_type === "N/A" && <p className="text-sm"> N/A </p>}
+                {transmission_type === "N/A" && (
+                  <p className="text-sm"> N/A </p>
+                )}
               </div>
             </div>
           </div>
@@ -89,5 +141,3 @@ const UploadedCarsList = ({ data }) => {
 };
 
 export default UploadedCarsList;
-
-
